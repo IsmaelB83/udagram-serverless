a3a5605df5f280da6e4bf6a80048a85f537ae933
Ismael Bernal <ismaelbernal83@gmail.com>
2022-02-09 07:36:47 +0100

    Add serverless configuration

---
+++ src/lambda/http/getGroups.ts
@@ -0,0 +1,98 @@
+// Imports 
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+
+// Constants
+const DB_TABLE: string = process.env.GROUPS_TABLE || '';
+
+// Variables
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+// Lamdba
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    // Parse query parameters
+    const nextKey = parseNextKeyParameter(event);
+    const limit = parseLimitParameter(event) || 10;
+    // Query
+    const params = { 
+      TableName : DB_TABLE,
+      Limit: limit,
+      ExclusiveStartKey: nextKey
+    }
+    const result = await docClient.scan(params).promise()
+    console.log(result);
+    // Return results
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({
+        nextKey: encodeNextKey(result.LastEvaluatedKey),
+        Items: result.Items
+      })
+    }
+  } catch (err) {
+    return { 
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({
+        error: err
+      })
+    }
+  }
+}
+
+/**
+ * Get value of the limit parameter.
+ *
+ * @param {Object} event HTTP event passed to a Lambda function
+ *
+ * @returns {number} parsed "limit" parameter
+ */
+function parseLimitParameter(event) {
+  const limitStr = getQueryParameter(event, 'limit')
+  if (!limitStr) return undefined
+  const limit = parseInt(limitStr, 10)
+  if (limit <= 0) throw new Error('Limit should be positive')
+  return limit
+}
+
+/**
+ * Get value of the limit parameter.
+ *
+ * @param {Object} event HTTP event passed to a Lambda function
+ *
+ * @returns {Object} parsed "nextKey" parameter
+ */
+function parseNextKeyParameter(event) {
+  const nextKeyStr = getQueryParameter(event, 'nextKey')
+  if (!nextKeyStr) return undefined
+  const uriDecoded = decodeURIComponent(nextKeyStr)
+  return JSON.parse(uriDecoded)
+}
+
+/**
+ * Get a query parameter or return "undefined"
+ *
+ * @param {Object} event HTTP event passed to a Lambda function
+ * @param {string} name a name of a query parameter to return
+ *
+ * @returns {string} a value of a query parameter value or "undefined" if a parameter is not defined
+ */
+function getQueryParameter(event, name) {
+  const queryParams = event.queryStringParameters
+  if (!queryParams) return undefined
+  return queryParams[name]
+}
+
+/**
+ * Encode last evaluated key using
+ *
+ * @param {Object} lastEvaluatedKey a JS object that represents last evaluated key
+ *
+ * @return {string} URI encoded last evaluated key
+ */
+function encodeNextKey(lastEvaluatedKey) {
+  if (!lastEvaluatedKey) return null
+  return encodeURIComponent(JSON.stringify(lastEvaluatedKey))
+}
