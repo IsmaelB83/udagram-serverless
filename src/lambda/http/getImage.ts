c9ade7d8233e56ceee64c26e3f85fd91f9c6afd4
Ismael Bernal <ismaelbernal83@gmail.com>
2022-02-11 20:09:58 +0100

    Adding a secondary index on Images table, and creating a getImage endpoint

---
+++ src/lambda/http/getImage.ts
@@ -0,0 +1,61 @@
+// Imports 
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+
+// Constants
+const IMAGES_TABLE: string = process.env.IMAGES_TABLE || '';
+const IMAGES_ID_INDEX: string = process.env.IMAGES_ID_INDEX || '';
+
+// Variables
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+// Lamdba
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    // Parse path parameters
+    if (!event.pathParameters) {
+      return { 
+        statusCode: 500,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        body: JSON.stringify({
+          error: 'Image id mandatory'
+        })
+      }
+    }
+    // Query image
+    const imageId = event.pathParameters.imageId || '';
+    const result = await docClient.query({
+      TableName: IMAGES_TABLE,
+      IndexName: IMAGES_ID_INDEX,
+      KeyConditionExpression: 'imageId = :imageId',
+      ExpressionAttributeValues: {
+        ':imageId': imageId
+      }
+    }).promise();
+    if (!result.Count || !result.Items) {
+      return { 
+        statusCode: 404,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        body: JSON.stringify({
+          error: `ImageId ${imageId} not found`
+        })
+      }
+    }
+    // Return image
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({
+        Item: result.Items[0]
+      })
+    }
+  } catch (err) {
+    return { 
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({
+        error: err
+      })
+    }
+  }
+}
