e98b4ac9f1596e4f5fbe5592393cf8179295cae2
Ismael Bernal <ismaelbernal83@gmail.com>
2022-02-12 08:43:13 +0100

    Add frontend

---
+++ react-client/src/components/UdagramImage.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { Card, Image } from 'semantic-ui-react'
+import { ImageModel } from '../types/ImageModel'
+import './UdagramImage.css';
+
+interface ImageCardProps {
+  image: ImageModel
+}
+
+interface ImageCardState {}
+
+export class UdagramImage extends React.PureComponent<
+  ImageCardProps,
+  ImageCardState
+> {
+
+  render() {
+    return (
+      <Card className="UdagramImage" fluid color="red">
+        <Card.Content>
+          <Card.Header>{this.props.image.title}</Card.Header>
+          <Card.Description>{this.props.image.timestamp}</Card.Description>
+          {this.props.image.url && (
+            <Image src={this.props.image.url} />
+          )}
+        </Card.Content>
+      </Card>
+    )
+  }
+}
---
+++ react-client/src/config.ts
@@ -0,0 +1,1 @@
+export const apiEndpoint = 'https://8kq3hn73nb.execute-api.us-east-1.amazonaws.com/dev'
