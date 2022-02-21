66c1f6ca4bb38fe2f7b2089c6a6164e1b84b004f
Ismael Bernal <ismaelbernal83@gmail.com>
2022-02-21 08:02:12 +0100

    Refactor to pass S3 Images bucket notifications trough SNS. From old approach (S3 event-->Lambda) to new approach (S3 event-->SNS topic-->Lambda subscriber)

--- serverless/src/lambda/s3/sendNotification.ts
+++ serverless/src/lambda/s3/sendNotification.ts
@@ -1,4 +1,4 @@
-import { S3Handler, S3Event } from 'aws-lambda';
+import { SNSHandler, SNSEvent, S3Event } from 'aws-lambda';
 import 'source-map-support/register'
 import * as AWS from 'aws-sdk';
 
@@ -14,50 +14,65 @@ const CONNECTION_PARAMS = {
 
 const API_GATEWAY = new AWS.ApiGatewayManagementApi(CONNECTION_PARAMS);
 
-export const handler: S3Handler = async (event: S3Event) => {
+/**
+ * Process SNS Event
+ * @param event SNS Event received
+ */
+export const handler: SNSHandler = async (event: SNSEvent) => {
+  // Iterate trhough each sns record received in the event
+  for (const snsRecord of event.Records) {
+    // Log
+    console.log('Processing SNS Record: ', snsRecord);
+    // The message receive in the SNS Event is an S3Event
+    const s3Event = JSON.parse(snsRecord.Sns.Message);
+    await processS3Event(s3Event);
+  }
+}
 
-  for (const record of event.Records) {
-    const key = record.s3.object.key;
-    console.log('Processing S3 item with key: ', key);
-
-    const connections = await DOC_CLIENT.scan({
-      TableName: CONNECTIONS_TABLE
-    }).promise();
-
-    if (connections.Items) {
-      const payload = {
-        imageId: key
-      }
-
+/**
+ * Process S3 events received in the SNS Event
+ * @param event S3 Event trigger by the bucket
+ */
+async function processS3Event (event: S3Event) {
+  // Log
+  console.log('Processing S3 event: ', event);
+  // Read current wss connections
+  const connections = await DOC_CLIENT.scan({
+    TableName: CONNECTIONS_TABLE
+  }).promise();
+  // If there are connections
+  if (connections.Items) {
+    // S3 Event could have more than one record
+    for (const record of event.Records) {
       for (const connection of connections.Items) {
         const connectionId = connection.id;
-        await sendMessageToClient(connectionId, payload); 
-        
-      }
+        await sendMessageToClient(connectionId, { imageId: record.s3.object.key });         
+      } 
     }
   }
 }
 
-async function sendMessageToClient(connectionId, payload) {
+/**
+ * Send message to a connected client
+ * @param connectionId WSS Connection id
+ * @param payload Message to send
+ */
+async function sendMessageToClient(connectionId: string, payload: any) {
   try {
+    // Post message to the wss connection
     console.log('Sending message to a connection: ', connectionId);
-
     await API_GATEWAY.postToConnection({
       ConnectionId: connectionId,
       Data: JSON.stringify(payload)
     }).promise();
-
   } catch (error) {
+    // If there is an exception deletes connectionId from DynamoDB
     console.log('Failed to send the message: ', error)
-
     if (error.statusCode === 410) {
       console.log('Stale connection');
-      
       await DOC_CLIENT.delete({
         TableName: CONNECTIONS_TABLE,
-        Key: {
-          id: connectionId
-        }
+        Key: { id: connectionId }
       });
     }
   }
